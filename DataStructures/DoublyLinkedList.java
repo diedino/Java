a586460f6138a71e3ec134d94f7b3453c1d9c9bb
diedino <vasiliykrasnov43@gmail.com>
2019-10-10 12:01:58 +0300

    just started

--- DataStructures/DoublyLinkedList.java
+++ DataStructures/DoublyLinkedList.java
@@ -1,2 +1,44 @@
-public class DoublyLinkedList {
+public class DoublyLinkedList<T> {
+    private int size;
+    private Node<T> head, tail;
+
+    public DoublyLinkedList() {
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Node<T> {
+
+        T value;
+        Node<T> next, previous;
+
+        public Node() {
+
+        }
+
+        public Node(T value) {
+            this.value = value;
+            next = null;
+            previous = null;
+        }
+        public boolean hasNext() {
+            return next!=null;
+        }
+        public boolean hasPrevious() {
+            return previous!=null;
+        }
+    }
 }
