0fb6374055df7d9e69484666e79a0eea00f9400b
diedino <vasiliykrasnov43@gmail.com>
2019-10-11 15:23:02 +0300

    exclude main

--- Algebra/BinPow.java
+++ Algebra/BinPow.java
@@ -29,14 +29,3 @@ public class BinPow {
     }
 }
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("a = ");
-        int a = sc.nextInt();
-        System.out.print("b = ");
-        int b = sc.nextInt();
-        System.out.println("with recursion: " + BinPow.binpowrecursion(a,b));
-        System.out.println("binpow: " + BinPow.binpow(a,b));
-    }
-}
